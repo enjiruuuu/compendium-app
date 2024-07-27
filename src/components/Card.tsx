7b0d9fb7d490b0079ffdd08d5e9734806c2db3d6
enjiruuuu <fightingdreamer99@gmail.com>
2024-07-28 02:50:00 +0800

    implement visual differentiation between seen and unseen animals

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,14 @@ import { Link } from "react-router-dom";
 import { IAnimal } from "../interfaces/IAnimal";
 import '../styles/Card.scss';
 
-export default function Card({ animal, id }: { animal: IAnimal, id: string }) {
-    return <div className="card">
+export default function Card({ animal, id, seen }: { animal: IAnimal, id: string, seen: boolean }) {
+    let classes: string = 'card';
+
+    if (seen) {
+        classes += ' seen';
+    }
+
+    return <div className={classes}>
         <Link to={`details/${id}`}>
             <div className="image" style={{
                 backgroundImage: `url(${animal.thumbnail})`
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,11 +3,14 @@ import '../styles/Home.scss';
 
 const Home = () => {
     const animals = require('../data/animals.json');
+    const seen = require('../data/seen.json');
+    const seenAnimals = new Set<string>(seen);
 
     return <div className="app-home">
-        {Object.keys(animals).map(key => (
-            <Card animal={animals[key]} id={key} key={key} />
-        ))}
+        {Object.keys(animals).map((key) => {
+            const seen: boolean = (seenAnimals.has(key)) ? true : false;
+            return <Card animal={animals[key]} id={key} seen={seen} key={key} />
+        })}
     </div>;
 };
 
